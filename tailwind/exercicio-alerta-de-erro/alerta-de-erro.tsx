eeefda57d3e8290c58d8b96a9822c29e9a257445
Eliezer <eliezerladeira@yahoo.com.br>
2025-07-15 18:51:55 -0300

    Exercício Alerta de Erro

---
+++ tailwind/exercicio-alerta-de-erro/alerta-de-erro.tsx
@@ -0,0 +1,12 @@
+const Page = () => {
+    return {
+        <div className="w-screen h-screen p-5">
+            <div className="bg-yellow-100 border-l-4 border-yellow-500 p-3 text-yellow-700">
+                Você não tem mais acesso.
+                <a href="" className="underline ml-1">Faça o upgrade agora!</a>
+            </div>
+        </div>
+    };
+}
+
+export default Page;
