5241af8b70a5e24b8387772cc8a92decb25449cf
Eliezer <eliezerladeira@yahoo.com.br>
2025-07-14 22:15:34 -0300

    Exercício: Botão

---
+++ tailwind/exercicio-botao/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+        <div className="w-screen h-screen p-5">
+            <div className="inline-block cursor-pointer bg-blue-500 text-white rounded-md px-5 py-2 border-none">Clique aqui</div>
+            <div className="inline-block cursor-pointer px-5 py-2 rounded-md border border-blue-500 text-white">Clique aqui</div>
+        </div>
+    );
+}
+
+export default Page;
